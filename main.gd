3e6d0efdb22995c3093cccd4e4d611fbeb664d88
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-16 00:10:00 +0100

    2024-12-16--00h10m00s

--- main.gd
+++ main.gd
@@ -196,4 +196,5 @@ func _on_compare_pressed():
 	if drawn_symbol.lines.size() > 0:
 		compress_symbol_drawn(drawn_symbol)
 		symbol_to_compare = drawn_symbol
+		symbol_to_draw_over = drawn_symbol
 		compare_index = null
