b59e5f6547a17bef8358aa32100f5ca889e5617b
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-15 23:43:48 +0100

    2024-12-15--23h43m48s

--- main.gd
+++ main.gd
@@ -112,8 +112,10 @@ func _process(_delta):
 	if compare_index != null:
 		var test = stored_symbols[0] as Symbol_Drawn
 		var matched_points = 0
+		var total_points = 0
 		for line in symbol_to_compare.compressed_lines:
 			for point in line:
+				total_points += 1
 				for compare_line in test.compressed_lines:
 					for compare_point in compare_line:
 						if (compare_point - point ).length() < 2.0:
