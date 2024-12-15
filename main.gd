6d11186b6c49f6bccfc3ac72cbf9f0654b4915c9
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-15 23:45:07 +0100

    2024-12-15--23h45m07s

--- main.gd
+++ main.gd
@@ -120,7 +120,7 @@ func _process(_delta):
 					for compare_point in compare_line:
 						if (compare_point - point ).length() < 2.0:
 							matched_points += 1
-				
+		print( "match ratio: ", matched_points * 100.0 / total_points)
 		test.compressed_lines
 
 
