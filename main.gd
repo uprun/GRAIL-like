17924d8fa86de1685e94ab231c1f39a38c49bb08
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-19 15:12:47 +0100

    2024-12-19--15h12m47s

--- main.gd
+++ main.gd
@@ -114,7 +114,6 @@ func _on_button_pressed():
 	var drawn_symbol = Symbol_Drawn.new()
 	drawn_symbol.lines = lines
 	lines = []
-	all_sub_paths = []
 	if drawn_symbol.lines.size() > 0:
 		drawn_symbol.prepare_rescaled_lines()
 		stored_symbols.push_back(drawn_symbol)
