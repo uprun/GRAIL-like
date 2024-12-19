71c51630217cc10bc1e808d1fbc33e943ddfd892
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-19 13:08:53 +0100

    2024-12-19--13h08m53s

--- Symbol_Drawn.gd
+++ Symbol_Drawn.gd
@@ -42,3 +42,7 @@ func compare_internal(two: Symbol_Drawn):
 	return match_ratio
 	
 
+func compare(two: Symbol_Drawn):
+	var match_ratio = compare_internal(self, two)
+	#reverse comparison
+	match_ratio = min(match_ratio, two.compare_internal(self))
