ee4b49b631e66800290572c49b3e6d4b38416b26
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-19 12:46:30 +0100

    2024-12-19--12h46m30s

--- Sub_Path.gd
+++ Sub_Path.gd
@@ -1,5 +1,6 @@
 class_name Sub_Path extends Object
-	var Start: Vector2
-	var Finish: Vector2
-	var Id: int
-	var Intersecting: bool
+
+var Start: Vector2
+var Finish: Vector2
+var Id: int
+var Intersecting: bool
