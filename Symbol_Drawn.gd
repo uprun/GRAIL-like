60e299c6fdace26fc98e7287bc8663bc70c150be
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-20 17:39:32 +0200

    cleanup code and better smoothness (#4)

--- Symbol_Drawn.gd
+++ Symbol_Drawn.gd
@@ -24,16 +24,35 @@ func prepare_rescaled_lines():
 	print("scale :", scale)
 	for line in self.lines:
 		self.compressed_lines.push_back([])
+		var previous_point = null
 		for point in line:
 			var x = (point.x - left.x) / scale
 			var y = (point.y - top.y) / scale
-			self.compressed_lines.back().push_back(Vector2(x, y))
-			
+			var to_add = Vector2(x, y)
+			if previous_point != null:
+				self.add_a_lot_of_points_in_between(previous_point, to_add)
+			self.compressed_lines.back().push_back(to_add)
+			previous_point = to_add
+		
+	
+func add_a_lot_of_points_in_between(start: Vector2, finish: Vector2):
+	var direction = finish - start
+	var length_of_direction = direction.length()
+	if length_of_direction <= 1:
+		return
+	
+	for i in range(0, length_of_direction, 1):
+		var to_add = start + (direction * i / length_of_direction)
+		self.compressed_lines.back().push_back(to_add)
+	
 func compare_internal(two: Symbol_Drawn):
 	var one = self
-	var matched_points = 0
-	var total_points = 0
+	
+	var match_ratio = 100.0
+	
 	for line in one.compressed_lines:
+		var matched_points = 0
+		var total_points = 0
 		for point in line:
 			total_points += 1
 			for compare_line in two.compressed_lines:
@@ -41,7 +60,9 @@ func compare_internal(two: Symbol_Drawn):
 					if (compare_point - point ).length() < 2.0:
 						matched_points += 1
 						break
-	var match_ratio = matched_points * 100.0 / total_points
+		var sub_match_ratio = matched_points * 100.0 / total_points
+		# each line (stroke) in how symbol is drawn is important
+		match_ratio = min(match_ratio, sub_match_ratio)
 	return match_ratio
 	
 
--- main.gd
+++ main.gd
@@ -31,32 +31,16 @@ func draw_compressed_symbol(symbol: Symbol_Drawn, offset: Vector2, color):
 			draw_circle(initial + offset, width, color)
 
 var stored_symbols = []
-var all_sub_paths = []
-
-func add_sub_path(start, finish):
-	var sub = Sub_Path.new()
-	sub.Start = start
-	sub.Finish = finish
-	sub.Id = len(all_sub_paths)
-	sub.Intersecting = false
-	all_sub_paths.push_back(sub)
 
 func _unhandled_input(event):
 	var mouse_position = get_viewport().get_mouse_position()
 	if Input.is_mouse_button_pressed( 1 ):
 		if current_line.is_empty():
-			
-			current_line_length = 0
-			add_sub_path(mouse_position, mouse_position)
 			current_line.push_back(mouse_position)
 		else:
 			var last_point = current_line.back()
 			if mouse_position != last_point:
 				current_line.push_back(mouse_position)
-				add_sub_path(last_point, mouse_position)
-				var dist  = last_point.distance_to(mouse_position)
-				current_line_length += dist
-				
 	else:
 		if not current_line.is_empty():
 			lines.push_back(current_line)
@@ -101,25 +85,17 @@ func _ready():
 	pass # Replace with function body.
 
 func my_draw_polyline(line: Array):
-	if line.is_empty():
-		pass
+	if len(line) == 0:
+		return
 	if len(line) > 1:
 		draw_polyline(line, color, width)
-	else:
-		draw_circle(line.back(), width, color)
-		
-func my_draw_sub(sub: Sub_Path):
-	var active_color = color
-	if sub.Intersecting:
-		active_color = Color.BLUE
-	if sub.Start.distance_to(sub.Finish) > 1:
-		draw_line(sub.Start, sub.Finish, active_color, width)
-	else:
-		draw_circle(sub.Start, width, active_color)
+	draw_circle(line.front(), max(1,width - (len(line)/5.0)), color)
 
 func _draw():
-	for sub in all_sub_paths:
-		my_draw_sub(sub)
+	for line in lines:
+		my_draw_polyline(line)
+	
+	my_draw_polyline(current_line)
 		
 	var offset = Vector2(50,120)
 	for i in len(stored_symbols):
@@ -137,7 +113,6 @@ func _on_button_pressed():
 	var drawn_symbol = Symbol_Drawn.new()
 	drawn_symbol.lines = lines
 	lines = []
-	all_sub_paths = []
 	if drawn_symbol.lines.size() > 0:
 		drawn_symbol.prepare_rescaled_lines()
 		stored_symbols.push_back(drawn_symbol)
@@ -160,7 +135,6 @@ func _on_compare_pressed():
 	var drawn_symbol = Symbol_Drawn.new()
 	drawn_symbol.lines = lines
 	lines = []
-	all_sub_paths = []
 	if drawn_symbol.lines.size() > 0:
 		drawn_symbol.prepare_rescaled_lines()
 		print()
